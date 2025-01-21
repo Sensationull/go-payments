9d1c899f589a772e9e096aee2db9a4c3444ca531
Andrew Johnson <andrewjohnso17@gmail.com>
2025-01-21 14:27:28 -0800

    left off at adding responses to endpoints

---
+++ src/gotutorial/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
+	http.HandleFunc("/health", handleHealth)
+
+	log.Println("Listening on localhost:4242...")
+	var err error = http.ListenAndServe("localhost:4242", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Handle Create Payment Intent Request Called")
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Handle Health Request Called")
+}
